874681567f1e40e9314677897490dbcebd545fc4
Zergity <zergity@gmail.com>
2019-04-10 15:12:36 +0700

    Update owner to gonex.

--- consensus/dccs/feeder.go
+++ consensus/dccs/feeder.go
@@ -1,18 +1,18 @@
-// Copyright 2015 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// Copyright 2015 The gonex Authors
+// This file is part of the gonex library.
 //
-// The go-ethereum library is free software: you can redistribute it and/or modify
+// The gonex library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// The go-ethereum library is distributed in the hope that it will be useful,
+// The gonex library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
-// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+// along with the gonex library. If not, see <http://www.gnu.org/licenses/>.
 
 package dccs
 
