a5a0d7968432f9570f7948f7e494c8a670cd10ba
Zergity <zergity@gmail.com>
2019-05-24 17:25:02 +0700

    ethstats: import ordering

--- ethstats/ethstats.go
+++ ethstats/ethstats.go
@@ -30,11 +30,10 @@ import (
 	"strings"
 	"time"
 
-	"github.com/ethereum/go-ethereum/consensus/dccs"
-
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/mclock"
 	"github.com/ethereum/go-ethereum/consensus"
+	"github.com/ethereum/go-ethereum/consensus/dccs"
 	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/eth"
