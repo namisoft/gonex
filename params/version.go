bbc5d168e0794fe6452dd16580edaa067209d261
Zergity <zergity@gmail.com>
2019-05-30 12:14:00 +0700

    params: temporary set the version meta to hotfix

--- params/version.go
+++ params/version.go
@@ -25,7 +25,7 @@ const (
 	VersionMinor = 0 // Minor version component of the current release
 	VersionPatch = 2 // Patch version component of the current release
 
-	VersionMeta = "stable" // Version metadata to append to the version string
+	VersionMeta = "hotfix2" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
