4ba66bb16ac37adb52ec848af98d939092f14025
Zergity <zergity@gmail.com>
2019-08-14 15:18:20 +0700

    build: only create fake Go workspace when nessessary

--- build/env.sh
+++ build/env.sh
@@ -7,24 +7,26 @@ if [ ! -f "build/env.sh" ]; then
     exit 2
 fi
 
-# Create fake Go workspace if it doesn't exist yet.
-workspace="$PWD/build/_workspace"
-root="$PWD"
-ethdir="$workspace/src/github.com/ethereum"
-if [ ! -L "$ethdir/go-ethereum" ]; then
-    mkdir -p "$ethdir"
-    cd "$ethdir"
-    ln -s ../../../../../. go-ethereum
-    cd "$root"
+if [ "`basename $PWD`" != "go-ethereum" ]; then
+    # Create fake Go workspace if it doesn't exist yet.
+    workspace="$PWD/build/_workspace"
+    root="$PWD"
+    ethdir="$workspace/src/github.com/ethereum"
+    if [ ! -L "$ethdir/go-ethereum" ]; then
+        mkdir -p "$ethdir"
+        cd "$ethdir"
+        ln -s ../../../../../. go-ethereum
+        cd "$root"
+    fi
+
+    # Set up the environment to use the workspace.
+    GOPATH="$workspace"
+    export GOPATH
+
+    # Run the command inside the workspace.
+    cd "$ethdir/go-ethereum"
+    PWD="$ethdir/go-ethereum"
 fi
-
-# Set up the environment to use the workspace.
-GOPATH="$workspace"
-export GOPATH
-
-# Run the command inside the workspace.
-cd "$ethdir/go-ethereum"
-PWD="$ethdir/go-ethereum"
 
 # Launch the arguments with the configured environment.
 exec "$@"
