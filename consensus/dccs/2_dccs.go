536b512537194fa114408b8be37a420a9acb47e2
Zergity <zergity@gmail.com>
2019-08-19 13:17:30 +0700

    consensus/dccs: clone the active consensus code to track the new changes

--- consensus/dccs/2_dccs.go
+++ consensus/dccs/2_dccs.go
@@ -18,9 +18,16 @@
 package dccs
 
 import (
+	"bytes"
+	"time"
+
+	"github.com/ethereum/go-ethereum/accounts"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/consensus"
+	"github.com/ethereum/go-ethereum/consensus/misc"
 	"github.com/ethereum/go-ethereum/core/state"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/log"
 )
 
 // Init the second hardfork of DCCS consensus
@@ -34,7 +41,52 @@ func (d *Dccs) init2() *Dccs {
 // looking those up from the database. This is useful for concurrently verifying
 // a batch of new headers.
 func (d *Dccs) verifyHeader2(chain consensus.ChainReader, header *types.Header, parents []*types.Header) error {
-	return d.verifyHeader1(chain, header, parents)
+	if header.Number == nil {
+		return errUnknownBlock
+	}
+	number := header.Number.Uint64()
+
+	// Don't waste time checking blocks from the future
+	if header.Time > uint64(time.Now().Unix()) {
+		return consensus.ErrFutureBlock
+	}
+
+	checkpoint := d.config.IsCheckpoint(number)
+	// Check that the extra-data contains both the vanity and signature
+	if len(header.Extra) < extraVanity {
+		return errMissingVanity
+	}
+	if len(header.Extra) < extraVanity+extraSeal {
+		return errMissingSignature
+	}
+	// Ensure that the extra-data contains a signer list on checkpoint, but none otherwise
+	signersBytes := len(header.Extra) - extraVanity - extraSeal
+	if !checkpoint && signersBytes != 0 {
+		return errExtraSigners
+	}
+	if checkpoint && signersBytes%common.AddressLength != 0 {
+		return errInvalidCheckpointSigners
+	}
+	// Ensure that the mix digest is zero as we don't have fork protection currently
+	if header.MixDigest != (common.Hash{}) {
+		return errInvalidMixDigest
+	}
+	// Ensure that the block doesn't contain any uncles which are meaningless in PoA
+	if header.UncleHash != uncleHash {
+		return errInvalidUncleHash
+	}
+	// Ensure that the block's difficulty is meaningful (may not be correct at this point)
+	if number > 0 {
+		if header.Difficulty == nil {
+			return errInvalidDifficulty
+		}
+	}
+	// If all checks passed, validate any special fields for hard forks
+	if err := misc.VerifyForkHashes(chain.Config(), header, false); err != nil {
+		return err
+	}
+	// All basic checks passed, verify cascading fields
+	return d.verifyCascadingFields2(chain, header, parents)
 }
 
 // verifyCascadingFields2 verifies all the header fields that are not standalone,
@@ -42,7 +94,42 @@ func (d *Dccs) verifyHeader2(chain consensus.ChainReader, header *types.Header, 
 // in a batch of parents (ascending order) to avoid looking those up from the
 // database. This is useful for concurrently verifying a batch of new headers.
 func (d *Dccs) verifyCascadingFields2(chain consensus.ChainReader, header *types.Header, parents []*types.Header) error {
-	return d.verifyCascadingFields1(chain, header, parents)
+	// The genesis block is the always valid dead-end
+	number := header.Number.Uint64()
+	if number == 0 {
+		return nil
+	}
+	// Ensure that the block's timestamp isn't too close to it's parent
+	var parent *types.Header
+	if len(parents) > 0 {
+		parent = parents[len(parents)-1]
+	} else {
+		parent = chain.GetHeader(header.ParentHash, number-1)
+	}
+	if parent == nil || parent.Number.Uint64() != number-1 || parent.Hash() != header.ParentHash {
+		return consensus.ErrUnknownAncestor
+	}
+	if parent.Time+d.config.Period > header.Time {
+		return ErrInvalidTimestamp
+	}
+	// Retrieve the snapshot needed to verify this header and cache it
+	snap, err := d.snapshot1(chain, header, parents)
+	if err != nil {
+		return err
+	}
+	// If the block is a checkpoint block, verify the signer list
+	if d.config.IsCheckpoint(number) {
+		signers := make([]byte, len(snap.Signers)*common.AddressLength)
+		for i, signer := range snap.signers1() {
+			copy(signers[i*common.AddressLength:], signer.Address[:])
+		}
+		extraSuffix := len(header.Extra) - extraSeal
+		if !bytes.Equal(header.Extra[extraVanity:extraSuffix], signers) {
+			return errInvalidCheckpointSigners
+		}
+	}
+	// All basic checks passed, verify the seal and return
+	return d.verifySeal2(chain, header, parents)
 }
 
 // verifySeal2 checks whether the signature contained in the header satisfies the
@@ -50,29 +137,198 @@ func (d *Dccs) verifyCascadingFields2(chain consensus.ChainReader, header *types
 // headers that aren't yet part of the local blockchain to generate the snapshots
 // from.
 func (d *Dccs) verifySeal2(chain consensus.ChainReader, header *types.Header, parents []*types.Header) error {
-	return d.verifySeal1(chain, header, parents)
+	// Verifying the genesis block is not supported
+	number := header.Number.Uint64()
+	if number == 0 {
+		return errUnknownBlock
+	}
+	// Retrieve the snapshot needed to verify this header and cache it
+	snap, err := d.snapshot1(chain, header, parents)
+	if err != nil {
+		return err
+	}
+
+	// Resolve the authorization key and check against signers
+	signer, err := ecrecover(header, d.signatures)
+	if err != nil {
+		return err
+	}
+	if _, ok := snap.Signers[signer]; !ok {
+		return errUnauthorizedSigner
+	}
+
+	headers, err := d.GetRecentHeaders(snap, chain, header, parents)
+	if err != nil {
+		return err
+	}
+	for _, h := range headers {
+		sig, err := ecrecover(h, d.signatures)
+		if err != nil {
+			return err
+		}
+		if signer == sig {
+			// Signer is among recents, only fail if the current block doesn't shift it out
+			return errRecentlySigned
+		}
+	}
+
+	var parent *types.Header
+	if len(headers) > 0 {
+		parent = headers[0]
+	}
+
+	// Ensure that the difficulty corresponds to the turn-ness of the signer
+	signerDifficulty := snap.difficulty(signer, parent)
+	if header.Difficulty.Uint64() != signerDifficulty {
+		return errInvalidDifficulty
+	}
+	return nil
 }
 
 // prepare2 implements consensus.Engine, preparing all the consensus fields of the
 // header for running the transactions on top.
 func (d *Dccs) prepare2(chain consensus.ChainReader, header *types.Header) error {
-	return d.prepare1(chain, header)
+	header.Nonce = types.BlockNonce{}
+	d.prepareBeneficiary(header, chain)
+
+	// Set the correct difficulty
+	snap, err := d.snapshot1(chain, header, nil)
+	if err != nil {
+		return err
+	}
+	number := header.Number.Uint64()
+	parent := chain.GetHeader(header.ParentHash, number-1)
+	if parent == nil {
+		return consensus.ErrUnknownAncestor
+	}
+	header.Difficulty = CalcDifficulty1(snap, d.signer, parent)
+	log.Trace("header.Difficulty", "difficulty", header.Difficulty)
+
+	// Ensure the extra data has all it's components
+	if len(header.Extra) < extraVanity {
+		header.Extra = append(header.Extra, bytes.Repeat([]byte{0x00}, extraVanity-len(header.Extra))...)
+	}
+	header.Extra = header.Extra[:extraVanity]
+
+	if d.config.IsCheckpoint(number) {
+		for _, signer := range snap.signers1() {
+			header.Extra = append(header.Extra, signer.Address[:]...)
+		}
+	}
+	header.Extra = append(header.Extra, make([]byte, extraSeal)...)
+
+	// Mix digest is reserved for now, set to empty
+	header.MixDigest = common.Hash{}
+
+	// Ensure the timestamp has the correct delay
+	header.Time = parent.Time + d.config.Period
+	if header.Time < uint64(time.Now().Unix()) {
+		header.Time = uint64(time.Now().Unix())
+	}
+	return nil
 }
 
 // finalize2 implements consensus.Engine, ensuring no uncles are set, nor block
 // rewards given, and returns the final block.
 func (d *Dccs) finalize2(chain consensus.ChainReader, header *types.Header, state *state.StateDB, txs []*types.Transaction, uncles []*types.Header) {
-	d.finalize1(chain, header, state, txs, uncles)
+	// Calculate any block reward for the sealer and commit the final state root
+	d.calculateRewards(chain, state, header)
+	header.Root = state.IntermediateRoot(chain.Config().IsEIP158(header.Number))
+	header.UncleHash = types.CalcUncleHash(nil)
 }
 
 // finalizeAndAssemble2 implements consensus.Engine, ensuring no uncles are set, nor block
 // rewards given, and returns the final block.
 func (d *Dccs) finalizeAndAssemble2(chain consensus.ChainReader, header *types.Header, state *state.StateDB, txs []*types.Transaction, uncles []*types.Header, receipts []*types.Receipt) (*types.Block, error) {
-	return d.finalizeAndAssemble1(chain, header, state, txs, uncles, receipts)
+	// Calculate any block reward for the sealer and commit the final state root
+	d.calculateRewards(chain, state, header)
+	header.Root = state.IntermediateRoot(chain.Config().IsEIP158(header.Number))
+	header.UncleHash = types.CalcUncleHash(nil)
+
+	// Assemble and return the final block for sealing
+	return types.NewBlock(header, txs, nil, receipts), nil
 }
 
 // seal2 implements consensus.Engine, attempting to create a sealed block using
 // the local signing credentials.
 func (d *Dccs) seal2(chain consensus.ChainReader, block *types.Block, results chan<- *types.Block, stop <-chan struct{}) error {
-	return d.seal1(chain, block, results, stop)
+	header := block.Header()
+
+	// Sealing the genesis block is not supported
+	number := header.Number.Uint64()
+	if number == 0 {
+		return errUnknownBlock
+	}
+	// For 0-period chains, refuse to seal empty blocks (no reward but would spin sealing)
+	if d.config.Period == 0 && len(block.Transactions()) == 0 {
+		return errWaitTransactions
+	}
+	// Don't hold the signer fields for the entire sealing procedure
+	d.lock.RLock()
+	signer, signFn := d.signer, d.signFn
+	d.lock.RUnlock()
+
+	// Bail out if we're unauthorized to sign a block
+	snap, err := d.snapshot1(chain, header, nil)
+	if err != nil {
+		return err
+	}
+	if _, authorized := snap.Signers[signer]; !authorized {
+		return errUnauthorizedSigner
+	}
+	// If we're amongst the recent signers, wait for the next block
+	headers, err := d.GetRecentHeaders(snap, chain, header, nil)
+	if err != nil {
+		return err
+	}
+	for _, h := range headers {
+		sig, err := ecrecover(h, d.signatures)
+		if err != nil {
+			return err
+		}
+		if signer == sig {
+			// Signer is among recents
+			log.Info("Signed recently, must wait for others")
+			return nil
+		}
+	}
+	var parent *types.Header
+	if len(headers) > 0 {
+		parent = headers[0]
+	}
+	// Sweet, the protocol permits us to sign the block, wait for our time
+	delay := time.Unix(int64(header.Time), 0).Sub(time.Now()) // nolint: gosimple
+	if !snap.inturn1(signer, parent) {
+		// It's not our turn explicitly to sign, delay it a bit
+		offset, err := snap.offset(signer, parent)
+		if err != nil {
+			return err
+		}
+		wiggle := d.calcDelayTimeForOffset(offset)
+		delay += wiggle
+		log.Trace("Out-of-turn signing requested", "wiggle", common.PrettyDuration(wiggle))
+	}
+	// Sign all the things!
+	sighash, err := signFn(accounts.Account{Address: signer}, accounts.MimetypeClique, DccsRLP(header))
+	if err != nil {
+		return err
+	}
+	copy(header.Extra[len(header.Extra)-extraSeal:], sighash)
+	// Wait until sealing is terminated or delay timeout.
+	log.Trace("Waiting for slot to sign and propagate", "delay", common.PrettyDuration(delay))
+	go func() {
+		select {
+		case <-stop:
+			return
+		case <-time.After(delay):
+		}
+
+		select {
+		case results <- block.WithSeal(header):
+		default:
+			log.Warn("Sealing result is not read by miner", "sealhash", SealHash(header))
+		}
+	}()
+
+	return nil
 }
